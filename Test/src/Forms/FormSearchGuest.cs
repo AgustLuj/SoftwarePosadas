4d9e89f8dd76aacd3598698691c450293cc05476
agustin Lujan <0j30j2@gmail.com>
2018-09-03 08:31:19 -0300

    pushed

---
+++ Test/src/Forms/FormSearchGuest.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: lujan
+ * Date: 31/8/2018
+ * Time: 10:46
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace SP.Forms
+{
+	/// <summary>
+	/// Description of FormSearchGuest.
+	/// </summary>
+	public partial class FormSearchGuest : Form
+	{
+		public FormSearchGuest()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
