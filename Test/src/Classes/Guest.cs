c56d63b6c438e1e21b40dcb42e2156b431e3578e
Mateo Pidal <pidal.mateo@gmail.com>
2018-09-06 11:07:06 -0300

    Little fix

--- Test/src/Classes/Guest.cs
+++ Test/src/Classes/Guest.cs
@@ -1,29 +1,19 @@
 ﻿/*
  * Created By 42476232
  * On 06/09/2018
- * Time: 09:15 a.m.
+ * Time: 11:06 a.m.
  */
 using System;
-using System.Drawing;
-using System.Windows.Forms;
 
-namespace Program.Classes
+namespace SP.Classes
 {
   /// <summary>
   /// Description of Guest.
   /// </summary>
-  public partial class Guest : Form
+  public class Guest
   {
     public Guest()
     {
-      //
-      // The InitializeComponent() call is required for Windows Forms designer support.
-      //
-      InitializeComponent();
-      
-      //
-      // TODO: Add constructor code after the InitializeComponent() call.
-      //
     }
   }
 }
