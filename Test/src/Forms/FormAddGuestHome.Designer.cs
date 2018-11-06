52a7f02a23fdabc0b325ee2b50971d754ba92056
Mateo Pidal <pidal.mateo@gmail.com>
2018-11-06 11:18:21 -0300

    Lujiii!

--- Test/src/Forms/FormAddGuestHome.Designer.cs
+++ Test/src/Forms/FormAddGuestHome.Designer.cs
@@ -36,120 +36,139 @@ namespace Program.Forms
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.label_ficha = new MaterialSkin.Controls.MaterialLabel();
-			this.button_huesped = new MaterialSkin.Controls.MaterialFlatButton();
-			this.button_paciente = new MaterialSkin.Controls.MaterialFlatButton();
-			this.materialToolStripMenuItem1 = new MaterialSkin.Controls.MaterialToolStripMenuItem();
-			this.materialToolStripMenuItem2 = new MaterialSkin.Controls.MaterialToolStripMenuItem();
-			this.materialToolStripMenuItem3 = new MaterialSkin.Controls.MaterialToolStripMenuItem();
-			this.materialFlatButton2 = new MaterialSkin.Controls.MaterialFlatButton();
-			this.SuspendLayout();
-			// 
-			// label_ficha
-			// 
-			this.label_ficha.Depth = 0;
-			this.label_ficha.Font = new System.Drawing.Font("Roboto", 11F);
-			this.label_ficha.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-			this.label_ficha.Location = new System.Drawing.Point(39, 34);
-			this.label_ficha.MouseState = MaterialSkin.MouseState.HOVER;
-			this.label_ficha.Name = "label_ficha";
-			this.label_ficha.Size = new System.Drawing.Size(215, 32);
-			this.label_ficha.TabIndex = 0;
-			this.label_ficha.Text = "Ficha N° ";
-			// 
-			// button_huesped
-			// 
-			this.button_huesped.AutoSize = true;
-			this.button_huesped.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-			this.button_huesped.Depth = 0;
-			this.button_huesped.Icon = null;
-			this.button_huesped.Location = new System.Drawing.Point(74, 86);
-			this.button_huesped.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
-			this.button_huesped.MouseState = MaterialSkin.MouseState.HOVER;
-			this.button_huesped.Name = "button_huesped";
-			this.button_huesped.Primary = false;
-			this.button_huesped.selected = false;
-			this.button_huesped.Size = new System.Drawing.Size(145, 36);
-			this.button_huesped.TabIndex = 1;
-			this.button_huesped.Text = "+ Añadir Huesped";
-			this.button_huesped.UseVisualStyleBackColor = true;
-			this.button_huesped.Click += new System.EventHandler(this.Button_huespedClick);
-			// 
-			// button_paciente
-			// 
-			this.button_paciente.AutoSize = true;
-			this.button_paciente.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-			this.button_paciente.Depth = 0;
-			this.button_paciente.Icon = null;
-			this.button_paciente.Location = new System.Drawing.Point(375, 86);
-			this.button_paciente.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
-			this.button_paciente.MouseState = MaterialSkin.MouseState.HOVER;
-			this.button_paciente.Name = "button_paciente";
-			this.button_paciente.Primary = false;
-			this.button_paciente.selected = false;
-			this.button_paciente.Size = new System.Drawing.Size(149, 36);
-			this.button_paciente.TabIndex = 2;
-			this.button_paciente.Text = "+ Añadir Paciente";
-			this.button_paciente.UseVisualStyleBackColor = true;
-			this.button_paciente.Click += new System.EventHandler(this.Button_pacienteClick);
-			// 
-			// materialToolStripMenuItem1
-			// 
-			this.materialToolStripMenuItem1.AutoSize = false;
-			this.materialToolStripMenuItem1.Name = "materialToolStripMenuItem1";
-			this.materialToolStripMenuItem1.Size = new System.Drawing.Size(120, 30);
-			this.materialToolStripMenuItem1.Text = "materialToolStripMenuItem1";
-			// 
-			// materialToolStripMenuItem2
-			// 
-			this.materialToolStripMenuItem2.AutoSize = false;
-			this.materialToolStripMenuItem2.Name = "materialToolStripMenuItem2";
-			this.materialToolStripMenuItem2.Size = new System.Drawing.Size(120, 30);
-			this.materialToolStripMenuItem2.Text = "materialToolStripMenuItem2";
-			// 
-			// materialToolStripMenuItem3
-			// 
-			this.materialToolStripMenuItem3.AutoSize = false;
-			this.materialToolStripMenuItem3.Name = "materialToolStripMenuItem3";
-			this.materialToolStripMenuItem3.Size = new System.Drawing.Size(120, 30);
-			this.materialToolStripMenuItem3.Text = "materialToolStripMenuItem3";
-			// 
-			// materialFlatButton2
-			// 
-			this.materialFlatButton2.AutoSize = true;
-			this.materialFlatButton2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-			this.materialFlatButton2.Depth = 0;
-			this.materialFlatButton2.Icon = null;
-			this.materialFlatButton2.Location = new System.Drawing.Point(513, 441);
-			this.materialFlatButton2.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
-			this.materialFlatButton2.MouseState = MaterialSkin.MouseState.HOVER;
-			this.materialFlatButton2.Name = "materialFlatButton2";
-			this.materialFlatButton2.Primary = false;
-			this.materialFlatButton2.selected = false;
-			this.materialFlatButton2.Size = new System.Drawing.Size(73, 36);
-			this.materialFlatButton2.TabIndex = 3;
-			this.materialFlatButton2.Text = "cerrar";
-			this.materialFlatButton2.UseVisualStyleBackColor = true;
-			this.materialFlatButton2.Click += new System.EventHandler(this.MaterialFlatButton2Click);
-			// 
-			// FormAddGuestHome
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.BackColor = System.Drawing.Color.White;
-			this.ClientSize = new System.Drawing.Size(741, 518);
-			this.Controls.Add(this.materialFlatButton2);
-			this.Controls.Add(this.button_paciente);
-			this.Controls.Add(this.button_huesped);
-			this.Controls.Add(this.label_ficha);
-			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-			this.Name = "FormAddGuestHome";
-			this.Text = "FormAddGuestHome";
-			this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.FormAddGuestHomeFormClosed);
-			this.Load += new System.EventHandler(this.FormAddGuestHomeLoad);
-			this.ResumeLayout(false);
-			this.PerformLayout();
+		  this.label_ficha = new MaterialSkin.Controls.MaterialLabel();
+		  this.button_huesped = new MaterialSkin.Controls.MaterialFlatButton();
+		  this.button_paciente = new MaterialSkin.Controls.MaterialFlatButton();
+		  this.materialToolStripMenuItem1 = new MaterialSkin.Controls.MaterialToolStripMenuItem();
+		  this.materialToolStripMenuItem2 = new MaterialSkin.Controls.MaterialToolStripMenuItem();
+		  this.materialToolStripMenuItem3 = new MaterialSkin.Controls.MaterialToolStripMenuItem();
+		  this.materialFlatButton2 = new MaterialSkin.Controls.MaterialFlatButton();
+		  this.materialRaisedButton1 = new MaterialSkin.Controls.MaterialRaisedButton();
+		  this.SuspendLayout();
+		  // 
+		  // label_ficha
+		  // 
+		  this.label_ficha.Depth = 0;
+		  this.label_ficha.Font = new System.Drawing.Font("Roboto", 11F);
+		  this.label_ficha.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+		  this.label_ficha.Location = new System.Drawing.Point(39, 34);
+		  this.label_ficha.MouseState = MaterialSkin.MouseState.HOVER;
+		  this.label_ficha.Name = "label_ficha";
+		  this.label_ficha.Size = new System.Drawing.Size(215, 32);
+		  this.label_ficha.TabIndex = 0;
+		  this.label_ficha.Text = "Ficha N° ";
+		  // 
+		  // button_huesped
+		  // 
+		  this.button_huesped.AutoSize = true;
+		  this.button_huesped.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+		  this.button_huesped.Depth = 0;
+		  this.button_huesped.Icon = null;
+		  this.button_huesped.Location = new System.Drawing.Point(74, 86);
+		  this.button_huesped.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+		  this.button_huesped.MouseState = MaterialSkin.MouseState.HOVER;
+		  this.button_huesped.Name = "button_huesped";
+		  this.button_huesped.Primary = false;
+		  this.button_huesped.selected = false;
+		  this.button_huesped.Size = new System.Drawing.Size(145, 36);
+		  this.button_huesped.TabIndex = 1;
+		  this.button_huesped.Text = "+ Añadir Huesped";
+		  this.button_huesped.UseVisualStyleBackColor = true;
+		  this.button_huesped.Click += new System.EventHandler(this.Button_huespedClick);
+		  // 
+		  // button_paciente
+		  // 
+		  this.button_paciente.AutoSize = true;
+		  this.button_paciente.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+		  this.button_paciente.Depth = 0;
+		  this.button_paciente.Icon = null;
+		  this.button_paciente.Location = new System.Drawing.Point(375, 86);
+		  this.button_paciente.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+		  this.button_paciente.MouseState = MaterialSkin.MouseState.HOVER;
+		  this.button_paciente.Name = "button_paciente";
+		  this.button_paciente.Primary = false;
+		  this.button_paciente.selected = false;
+		  this.button_paciente.Size = new System.Drawing.Size(149, 36);
+		  this.button_paciente.TabIndex = 2;
+		  this.button_paciente.Text = "+ Añadir Paciente";
+		  this.button_paciente.UseVisualStyleBackColor = true;
+		  this.button_paciente.Click += new System.EventHandler(this.Button_pacienteClick);
+		  // 
+		  // materialToolStripMenuItem1
+		  // 
+		  this.materialToolStripMenuItem1.AutoSize = false;
+		  this.materialToolStripMenuItem1.Name = "materialToolStripMenuItem1";
+		  this.materialToolStripMenuItem1.Size = new System.Drawing.Size(120, 30);
+		  this.materialToolStripMenuItem1.Text = "materialToolStripMenuItem1";
+		  // 
+		  // materialToolStripMenuItem2
+		  // 
+		  this.materialToolStripMenuItem2.AutoSize = false;
+		  this.materialToolStripMenuItem2.Name = "materialToolStripMenuItem2";
+		  this.materialToolStripMenuItem2.Size = new System.Drawing.Size(120, 30);
+		  this.materialToolStripMenuItem2.Text = "materialToolStripMenuItem2";
+		  // 
+		  // materialToolStripMenuItem3
+		  // 
+		  this.materialToolStripMenuItem3.AutoSize = false;
+		  this.materialToolStripMenuItem3.Name = "materialToolStripMenuItem3";
+		  this.materialToolStripMenuItem3.Size = new System.Drawing.Size(120, 30);
+		  this.materialToolStripMenuItem3.Text = "materialToolStripMenuItem3";
+		  // 
+		  // materialFlatButton2
+		  // 
+		  this.materialFlatButton2.AutoSize = true;
+		  this.materialFlatButton2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+		  this.materialFlatButton2.Depth = 0;
+		  this.materialFlatButton2.Icon = null;
+		  this.materialFlatButton2.Location = new System.Drawing.Point(513, 441);
+		  this.materialFlatButton2.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+		  this.materialFlatButton2.MouseState = MaterialSkin.MouseState.HOVER;
+		  this.materialFlatButton2.Name = "materialFlatButton2";
+		  this.materialFlatButton2.Primary = false;
+		  this.materialFlatButton2.selected = false;
+		  this.materialFlatButton2.Size = new System.Drawing.Size(73, 36);
+		  this.materialFlatButton2.TabIndex = 3;
+		  this.materialFlatButton2.Text = "cerrar";
+		  this.materialFlatButton2.UseVisualStyleBackColor = true;
+		  this.materialFlatButton2.Click += new System.EventHandler(this.MaterialFlatButton2Click);
+		  // 
+		  // materialRaisedButton1
+		  // 
+		  this.materialRaisedButton1.AutoSize = true;
+		  this.materialRaisedButton1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+		  this.materialRaisedButton1.Depth = 0;
+		  this.materialRaisedButton1.Icon = null;
+		  this.materialRaisedButton1.Location = new System.Drawing.Point(304, 234);
+		  this.materialRaisedButton1.MouseState = MaterialSkin.MouseState.HOVER;
+		  this.materialRaisedButton1.Name = "materialRaisedButton1";
+		  this.materialRaisedButton1.Primary = true;
+		  this.materialRaisedButton1.Size = new System.Drawing.Size(195, 36);
+		  this.materialRaisedButton1.TabIndex = 4;
+		  this.materialRaisedButton1.Text = "materialRaisedButton1";
+		  this.materialRaisedButton1.UseVisualStyleBackColor = true;
+		  this.materialRaisedButton1.Click += new System.EventHandler(this.MaterialRaisedButton1Click);
+		  // 
+		  // FormAddGuestHome
+		  // 
+		  this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+		  this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+		  this.BackColor = System.Drawing.Color.White;
+		  this.ClientSize = new System.Drawing.Size(741, 518);
+		  this.Controls.Add(this.materialRaisedButton1);
+		  this.Controls.Add(this.materialFlatButton2);
+		  this.Controls.Add(this.button_paciente);
+		  this.Controls.Add(this.button_huesped);
+		  this.Controls.Add(this.label_ficha);
+		  this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+		  this.Name = "FormAddGuestHome";
+		  this.Text = "FormAddGuestHome";
+		  this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.FormAddGuestHomeFormClosed);
+		  this.Load += new System.EventHandler(this.FormAddGuestHomeLoad);
+		  this.ResumeLayout(false);
+		  this.PerformLayout();
 		}
+		private MaterialSkin.Controls.MaterialRaisedButton materialRaisedButton1;
 		private MaterialSkin.Controls.MaterialFlatButton materialFlatButton2;
 		private MaterialSkin.Controls.MaterialToolStripMenuItem materialToolStripMenuItem3;
 		private MaterialSkin.Controls.MaterialToolStripMenuItem materialToolStripMenuItem2;
--- Test/src/Forms/FormAddGuestHome.cs
+++ Test/src/Forms/FormAddGuestHome.cs
@@ -31,6 +31,7 @@ namespace Program.Forms
 			lista = new List<MaterialSkin.Controls.MaterialFlatButton>();
 				lista.Add(button_huesped);
 				lista.Add(button_paciente);
+				
 		}
 		
 		void Button_huespedClick(object sender, EventArgs e)
@@ -93,28 +94,26 @@ namespace Program.Forms
 		}
 		
 	 	void createButton(String str, int x, int y){
+		  
+		  Console.WriteLine(String.Format("str: {0}, x: {1}, y: {2}", str, x, y));
 			
 			var btn = new MaterialFlatButton();	
 				btn.AutoSize = true;
 				btn.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
 				btn.Depth = 0;
-				btn.Icon = null;
 				btn.Location = new System.Drawing.Point(x,y);
-				btn.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
-				btn.MouseState = MaterialSkin.MouseState.HOVER;
 				btn.Name = (str == "+ añadir huesped ") ? "btn"+ count_btn1 : "btnP" + count_btn2;
 				btn.Primary = false;
 				btn.Size = new System.Drawing.Size(145, 36);
-				btn.TabIndex = 1;
 				btn.Text = str;
-				btn.UseVisualStyleBackColor = true;
 			
 			if(str == "+ Añadir huesped "){
 				btn.Click += new System.EventHandler(this.Button_huespedClick);
 			}else{
 				btn.Click += new System.EventHandler(this.MaterialFlatButton1Click);
 			}
-			(this.Parent as Panel).Controls.Add(btn);
+			this.Controls.Add(btn);
+			btn.BringToFront();
 			
 			lista.Add(btn);
 			
@@ -123,7 +122,9 @@ namespace Program.Forms
 			}else{
 				count_btn2++;
 			}
+		  Console.WriteLine("yep");
 		}
+		
 		public void ChangeBtn(string names,string a, string b){
 			/*for(int j = 0;j<this.Controls.Count;j++){
 				if(names == this.Controls[j].Name){
@@ -147,6 +148,11 @@ namespace Program.Forms
 		void FormAddGuestHomeLoad(object sender, EventArgs e)
 		{
 			
+		}
+		
+		void MaterialRaisedButton1Click(object sender, EventArgs e)
+		{
+		  addButton1(2);
 		}
 	}
 }
